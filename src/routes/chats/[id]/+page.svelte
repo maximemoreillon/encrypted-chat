c8ed33655064e21a1c95dc6f216835303ab51267
Maxime Moreillon <m.moreillon@gmail.com>
2026-01-04 11:10:46 +0900

    key check

--- src/routes/chats/[id]/+page.svelte
+++ src/routes/chats/[id]/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Input } from "$lib/components/ui/input/index.js";
   import { Button } from "$lib/components/ui/button/index.js";
-  import { encrypt } from "$lib/cryptography";
+  import { decrypt, encrypt } from "$lib/cryptography";
   import { onMount } from "svelte";
   import { Label } from "$lib/components/ui/label/index.js";
   import { Switch } from "$lib/components/ui/switch/index.js";
@@ -26,20 +26,36 @@
   let id = page.params.id;
 
   let keyBase64 = $state<string>("");
-
   let showDecrypted = $state(false);
-
   let messages = $state<DocumentData[]>([]);
   let chat = $state<DocumentData>();
+
+  // TODO: this is not nice, improve
+  let keyIsValid = $state(true);
 
   const subscribeToMessages = () => {
     if (!id) return alert("Missing chat ID");
     const collectionRef = collection(db, "chats", id, "messages");
     const q = query(collectionRef, orderBy("timestamp", "desc"));
-    onSnapshot(q, ({ docs }) => {
+
+    onSnapshot(q, async ({ docs }) => {
       messages = docs;
     });
   };
+
+  $effect(() => {
+    console.log("effect has run");
+    // Check if key is valid
+    // TODO: this is not nice, improve
+    // PROBLEM: will not run if
+    const lastMessage = messages.at(-1);
+    if (!lastMessage) return;
+    const { ciphertextBase64, ivBase64 } = lastMessage.data();
+    if (!keyBase64) return;
+    decrypt(ciphertextBase64, ivBase64, keyBase64)
+      .then(() => (keyIsValid = true))
+      .catch(() => (keyIsValid = false));
+  });
 
   const subscribeToChat = () => {
     if (!id) return alert("Missing chat ID");
@@ -70,7 +86,7 @@
       <Switch
         id="decrypted"
         bind:checked={showDecrypted}
-        disabled={!keyBase64}
+        disabled={!keyBase64 || !keyIsValid}
       />
       <Label for="decrypted">Decrypt</Label>
     </div>
@@ -84,8 +100,12 @@
       <Message {message} {showDecrypted} {keyBase64} />
     {/each}
   </div>
-  <MessageForm {chat} {keyBase64} />
-  {#if !keyBase64}
+
+  {#if !keyIsValid}
+    <div class="text-red-500 text-center">Key is not correct</div>
+  {:else if !keyBase64}
     <div class="text-red-500 text-center">Encryption key not set</div>
+  {:else}
+    <MessageForm {chat} {keyBase64} />
   {/if}
 {/if}
